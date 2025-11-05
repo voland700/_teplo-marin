2ebc683b425b5f5f49ea52afa34aba54cdce089f
voland700 <voland700@yandex.ru>
2025-11-05 17:38:20 +0300

    start product detail

--- app/js/app.js
+++ app/js/app.js
@@ -247,5 +247,29 @@ document.addEventListener('DOMContentLoaded', () => {
 		});
 	}
 
+	//*--- Counter for products list  ---*//
+	const counter = function () {
+		const btns = document.querySelectorAll('.card_counter__btn');
+		btns.forEach(btn => {
+			btn.addEventListener('click', function () {
+				const direction = this.dataset.direction;
+				const inp = this.parentElement.querySelector('.card_counter__value');
+				const currentValue = +inp.value;
+				let newValue;
+				if (direction === 'plus') {
+					newValue = currentValue + 1;
+				} else {
+					newValue = currentValue - 1 > 1 ? currentValue - 1 : 1;
+				}
+				if(typeof newValue !== NaN){
+					inp.value =  newValue;
+				} else {
+					inp.value =  1;
+				}
+			})
+		})
+	}
+	counter();
+
 
 }) /* -- END --*/
